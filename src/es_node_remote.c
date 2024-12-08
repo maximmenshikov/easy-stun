8261cd33fb81117139c3cbd00cfcd9d442f47600
Maxim Menshikov <maxim@menshikov.org>
2024-12-08 01:46:17 +0000

    Clean up binding

--- src/es_node_remote.c
+++ src/es_node_remote.c
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <errno.h>
 #include "es_node.h"
 #include "es_status.h"
 #include "es_bool.h"
@@ -27,8 +28,21 @@ fits(es_msg *msg, uint32_t appended_len)
 	return (msg->hdr->message_len + sizeof(*msg->hdr) + appended_len) <= msg->max_len;
 }
 
+static void
+es_msg_hdr_init(es_msg *msg, uint16_t type)
+{
+	msg->hdr->message_type = htons(type);
+	msg->hdr->magic_cookie = htonl(0x2112A442);
+    for (int i = 0; i < sizeof(msg->hdr->tid); ++i)
+    {
+        msg->hdr->tid[i] = (uint8_t)(rand() % 256);
+    }
+
+    msg->hdr->message_len = 0;
+}
+
 static es_status
-add_attr(es_msg *msg, stun_attr_type type, void *value, uint32_t value_len)
+es_msg_add_attr(es_msg *msg, stun_attr_type type, void *value, uint32_t value_len)
 {
 	stun_attr *attr;
 	size_t padding_len;
@@ -62,7 +76,7 @@ add_attr(es_msg *msg, stun_attr_type type, void *value, uint32_t value_len)
 }
 
 static es_status
-add_message_integrity(es_msg *msg, es_params *params)
+es_msg_add_integrity(es_msg *msg, es_params *params)
 {
 	uint32_t msg_len = msg->hdr->message_len + sizeof(*msg->hdr);
 	uint32_t padding = PAD(msg_len, 64);
@@ -73,7 +87,7 @@ add_message_integrity(es_msg *msg, es_params *params)
 
 	digest = HMAC(EVP_sha1(), params->password, strlen(params->password),
 		(const unsigned char *)msg->hdr, msg_len + padding, NULL, NULL);
-	return add_attr(msg, STUN_ATTR_MESSAGE_INTEGRITY, digest, 20);
+	return es_msg_add_attr(msg, STUN_ATTR_MESSAGE_INTEGRITY, digest, 20);
 }
 
 es_status
@@ -84,7 +98,6 @@ es_remote_bind(es_node *node, es_params *params)
 	struct sockaddr_in addr;
 	char req[4096] = { 0 };
 	int ret;
-	stun_hdr *hdr;
 	size_t offset;
 	es_msg msg;
 
@@ -102,47 +115,36 @@ es_remote_bind(es_node *node, es_params *params)
 	addr.sin_family = AF_INET;
 	memcpy(&(addr.sin_addr), host->h_addr_list[0], host->h_length);
 
-
-	hdr = (stun_hdr *)req;
-	hdr->message_type = htons(0x0001);
-	hdr->magic_cookie = htonl(0x2112A442);
-    for (int i = 0; i < sizeof(hdr->tid); ++i)
-    {
-        hdr->tid[i] = (uint8_t)(rand() % 256);
-    }
-
-    hdr->message_len = 0;
-
-    printf("b %d\n", hdr->message_len);
+	es_msg_hdr_init(&msg, 0x0001);
     EXIT_ON_ERROR("Failed to add username",
-    	          add_attr(&msg,
-    	                   STUN_ATTR_USERNAME,
-    	                   params->username,
-    	                   strlen(params->username)));
+    	          es_msg_add_attr(&msg,
+    	                          STUN_ATTR_USERNAME,
+    	                          params->username,
+    	                          strlen(params->username)));
     EXIT_ON_ERROR("Failed to add connection request binding",
-    	          add_attr(&msg,
-    	                   STUN_ATTR_CONN_REQUEST_BINDING,
-    	                   "dslforum.org/TR-111 ",
-    	                   20));
+    	          es_msg_add_attr(&msg,
+    	                          STUN_ATTR_CONN_REQUEST_BINDING,
+    	                          "dslforum.org/TR-111 ",
+    	                          20));
     EXIT_ON_ERROR("Failed to add binding change",
-    	          add_attr(&msg,
-    	                   STUN_ATTR_BINDING_CHANGE,
-    	                   "",
-    	                   0));
+    	          es_msg_add_attr(&msg,
+    	                          STUN_ATTR_BINDING_CHANGE,
+    	                          "",
+    	                          0));
     EXIT_ON_ERROR("Failed to add message integrity",
-    	          add_message_integrity(&msg, params));
+    	          es_msg_add_integrity(&msg, params));
 
-    printf("a %d\n", hdr->message_len);
-    hdr->message_len = htons(hdr->message_len);
-	ret = sendto(node->sk, req, ntohs(hdr->message_len) + sizeof(stun_hdr), 0,
+    msg.hdr->message_len = htons(msg.hdr->message_len);
+	ret = sendto(node->sk, req, ntohs(msg.hdr->message_len) + sizeof(stun_hdr), 0,
 		(struct sockaddr *)&addr, sizeof(addr));
-	dbg("Sent %d bytes", ret);
 	if (ret < 0)
 	{
+		err("Failed to send message: %s", strerror(errno));
 		rc = ES_ESENDFAIL;
 		goto err;
 	}
 
+	dbg("Sent %d bytes", ret);
 	return ES_EOK;
 
 err:
